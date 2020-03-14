52ae338c9887af063a83f3e5f174737a62599bd6
André <aa.concha7@gmail.com>
2020-03-14 12:22:31 -0300

    Primeiro commit projeto chat com SignalR

---
+++ WebChatSignalR.Web/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebChatSignalR.Web.Models
+{
+    public class Message
+    {
+        public Int64 destination { get; set; }
+        public User sender { get; set; }
+        public object message { get; set; }
+    }
+}
---
+++ WebChatSignalR.Web/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebChatSignalR.Web.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public Int64 key { get; set; }
+        public DateTime dtConnection { get; set; }
+    }
+}
